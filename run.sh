2a51c1f511c21bc1985ebe6726b42f2937469192
Ben Axnick <ben.axnick@flippa.com>
2016-03-26 22:28:40 -0700

    Make it possible to copy an existing map on server startup

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -z "${SAVE_FILE_URL}" ]; then
+  echo "Fetching save from ${SAVE_FILE_URL}"
+  curl -kL "${SAVE_FILE_URL}" > "saves/default.zip"
+fi
+
 if [ ! -f "/opt/factorio/saves/default.zip" ]; then
   echo "Creating a default Factorio save"
   /opt/factorio/bin/x64/factorio --create default
