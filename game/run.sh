e48ac832d0e639ab5febfa8f7c1af9070e6a96b7
Ben Axnick <ben.axnick@flippa.com>
2016-04-09 20:27:35 -0700

    Download both mods and save from remote locations

--- game/run.sh
+++ game/run.sh
@@ -2,7 +2,16 @@
 
 if [ ! -z "${SAVE_FILE_URL}" ]; then
   echo "Fetching save from ${SAVE_FILE_URL}"
+  remote_last_modified=$(curl -svX HEAD ${SAVE_FILE_URL} 2>&1 | grep '^< Last-Modified:')
+  echo "Remote last mod: ${remote_last_modified}"
   curl -kL "${SAVE_FILE_URL}" > "saves/default.zip"
+fi
+
+if [ ! -z "${MODS_FILE_URL}" ]; then
+  echo "Fetching mods from ${MODS_FILE_URL}"
+  curl -kL "${MODS_FILE_URL}" > "mods.zip"
+  rm -f mods/* && unzip mods.zip -d mods
+  rm mods.zip
 fi
 
 if [ ! -f "/opt/factorio/saves/default.zip" ]; then
